67e05b3e1a82a16dd841c147af35230eb90f0912
testden1303medved <126058089+testden1303medved@users.noreply.github.com>
2025-01-19 15:31:10 +1100

    Add files via upload

---
+++ dbil.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/center', methods=['POST'])
+def create_user():
+    character = request.json.get('x')
+    amount = request.json.get('y')
+    
+    if not character or not isinstance(character, str):
+        return jsonify({"error": "Invalid or missing 'x'"}), 400
+    if not amount or not isinstance(amount, int):
+        return jsonify({"error": "Invalid or missing 'y'"}), 400
+    
+    x = character * amount
+    
+    return jsonify({"result": x}), 200
+
+app.run(debug=True)
