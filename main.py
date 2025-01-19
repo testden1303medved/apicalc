270aa80a5b4bcee859023787247ee57bcba6c396
testden1303medved <126058089+testden1303medved@users.noreply.github.com>
2025-01-19 17:41:06 +1100

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/center', methods=['POST'])
-def create_user():
-    character = request.json.get('x')
-    amount = request.json.get('y')
+def center_calculator():
+    word  = str(request.json.get('x'))
+    width = int(request.json.get('y'))
+    fill  = str(request.json.get('z'))
     
-    if not character:
-        return jsonify({"error": "missing 'x'"}), 400
-    if not amount:
-        return jsonify({"error": "missing 'y'"}), 400
+    if not word : return jsonify({"error": "missing 'x'"}), 400
+    if not width: return jsonify({"error": "missing 'y'"}), 400
+    if not char : char = " "
     
-    x = character * (int(amount) * 4)
+    total_padding = max(0, width - len(word))
+    left_padding  = total_padding // 2
+    right_padding = total_padding - left_padding
     
-    return jsonify({"result": x}), 200
+    left_fill  = (fill * (left_padding // len(fill) + 1))[:left_padding]
+    right_fill = (fill * (right_padding // len(fill) + 1))[:right_padding]
 
-app.run(host='0.0.0.0', port=8080)
+    
+    return jsonify({"result": f"{left_fill}$$${right_fill}"}), 200
+
+if __name__ == "__main__":
+    app.run(port=8080)
